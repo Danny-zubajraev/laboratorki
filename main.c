b06451f1168cf7903f865e6a7e619de01ddec8b9
Danny-zubajraev <99704101+Danny-zubajraev@users.noreply.github.com>
2022-06-20 06:17:56 +0500

    lab5

--- main.c
+++ main.c
@@ -1,43 +1,29 @@
-#include <math.h>
 #include <stdio.h>
 
-double f (long int n)
+//рекурсивная функция
+unsigned int stR(unsigned int n, unsigned int a)
 {
-    double sum=0, x, y, h;
-    h = M_PI/n;
-    for (x=M_PI/2/n; x <= M_PI; x += h)
-    {
-        if (x > M_PI/2 && x < M_PI)
-            y = sqrt(x)*pow(M_E, -(x*x));
-        else
-            y = pow(2.0, x) - 2 + x*x;
-        sum = sum + y;
-    }
-    sum = sum*h;
-    return (sum);
+    if (n==0) return (1);
+    a = stR(n-1, a) * a;
+    return a;
 }
+
+unsigned int stC(unsigned int n, unsigned int a)
+{
+    unsigned int i, p = 1;
+    for (i=0; i<n; i++)
+        p *= a;
+    return p;
+
+}
+
 int main()
 {
-    double sum1, sum2 = 0, e;
-    long int n=1;
-
-    printf("vvedite tochnost\n");
-    scanf("%lf", &e);
-
-
-    while (1)
-    {
-        sum1 = f(n);
-
-      if((fabs(sum2-sum1)/3) < e)
-          break;
-      else
-      {
-          n *=2;
-          sum2 = sum1;
-      }
-
-    }
-    printf("I1 = %.5lf", sum1);
-    return 0;
+    unsigned int n, a;
+    printf("write the number and ^ \nnumber -> ");
+    scanf("%u", &a);
+    printf("^ -> ");
+    scanf("%u", &n);
+    printf("rec %u^%u = %u\n", a, n, stR(n, a));
+    printf("cycle %u^%u = %u\n", a, n, stC(n, a));
 }
